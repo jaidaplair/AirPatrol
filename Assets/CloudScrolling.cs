eec3c6d75a0b103c90ab04432be06c9fc25f4a19
jaidaplair <90574364+jaidaplair@users.noreply.github.com>
2024-02-17 18:26:23 -0500

    fixed scrolling of clouds

--- Assets/CloudScrolling.cs
+++ Assets/CloudScrolling.cs
@@ -3,74 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CloudScrolling : MonoBehaviour
-{/*
-    public float scrollSpeed = 1f;
-    public float resetPosition = 10f;
-
-    private Transform[] clouds;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Get the transform components of both cloud objects
-        clouds = new Transform[2];
-        clouds[0] = transform;
-        clouds[1] = transform.GetChild(0); // Assuming both clouds are children of the same parent
-
-    }
-
+{
+    [SerializeField] float speed = 2f;
+    [SerializeField] float resetPosition = 9f;
     // Update is called once per frame
     void Update()
     {
-        // Move clouds horizontally
-        foreach (Transform cloud in clouds)
-        {
-            cloud.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
-
-            // Reset position if cloud goes beyond reset position
-            if (cloud.position.x < -resetPosition)
-            {
-                ResetCloudPosition(cloud);
-            }
-        }
-
-    }
-
-    private void ResetCloudPosition(Transform cloud)
-    {
-        // Reposition the cloud just before the viewing window
-        float offset = Mathf.Abs(cloud.position.x) + resetPosition;
-        cloud.position = new Vector3(offset, cloud.position.y, cloud.position.z);
-    }*/
-    public float scrollSpeed = 1f;
-    public float resetPosition = 10f;
-
-    private void Update()
-    {
-        transform.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
-
-        if (transform.position.x < -resetPosition)
+        //move left across the screen
+        transform.Translate(Time.deltaTime * speed * Vector3.left);
+        if (transform.position.x < -resetPosition - 8)
         {
             ResetCloudPosition();
         }
     }
-
     private void ResetCloudPosition()
     {
+        // Reposition the cloud just before the viewing window
         float offset = Mathf.Abs(transform.position.x) + resetPosition;
         transform.position = new Vector3(offset, transform.position.y, transform.position.z);
     }
-
-
-
-
-
-
-
-
-
-
-
-
+    
 }
