8be9fc26ce45fe9f07baffbbfa26c3fa933cd4bd
jaidaplair <90574364+jaidaplair@users.noreply.github.com>
2024-02-17 20:09:55 -0500

    aim and shooting sound

---
+++ Assets/scripts/ReticleController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticleController : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    [SerializeField] AudioClip firingSound;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // Control reticle movement
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        // Wrap around screen edges
+        if (transform.position.x > 9f)
+        {
+            transform.position = new Vector3(-9f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -9f)
+        {
+            transform.position = new Vector3(9f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y > 5f)
+        {
+            transform.position = new Vector3(transform.position.x, -5f, transform.position.z);
+        }
+        else if (transform.position.y < -5f)
+        {
+            transform.position = new Vector3(transform.position.x, 5f, transform.position.z);
+        }
+
+        // Check for firing input
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        // Play firing sound
+        if (firingSound != null)
+        {
+            audioSource.PlayOneShot(firingSound);
+        }
+    }
+}
