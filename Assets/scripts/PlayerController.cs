26f0d2b79e50d3b1a49269a3876102cfcc822b95
Jaida Plair <jaiplair@gmail.com>
2024-02-28 09:46:32 -0500

    player plane

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    [SerializeField] AudioClip firingSound;
+    //made reference prefab of the explosion
+    [SerializeField] GameObject explosion;
+
+    // Start is called before the first frame update
+    private AudioSource audioSource;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        // Wrap around screen edges
+        if (transform.position.x > 7f)
+        {
+            transform.position = new Vector3(7f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -7f)
+        {
+            transform.position = new Vector3(-7f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y > 4.7f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.7f, transform.position.z);
+        }
+        else if (transform.position.y < -4.7f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.7f, transform.position.z);
+        }
+
+        // Check for firing input
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+            //function to make a explosion everytime space is pushed, returns a reference to the pbject that was created
+            Instantiate(explosion, transform.position, transform.rotation);
+            /*
+             //make a new baby everytime space is pushed, returns a reference to the pbject that was created
+            GameObject obj;
+            obj= Instantiate(babyPrefab);
+            //Vector3.zero is equivalent to new Vector3(0,0,0)
+            //spews babys out of the boss
+            obj.transform.position = transform.position;
+        */
+        }
+    }
+    void Fire()
+    {
+        // Play firing sound
+        if (firingSound != null)
+        {
+            audioSource.PlayOneShot(firingSound);
+
+        }
+    }
+}
